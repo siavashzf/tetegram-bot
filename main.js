b785797476d9e36590d654ea94a2698dc3ae48d8
siavashzf <zarif.siavash.offical@gmail.com>
2020-12-09 00:31:51 +0330

    hanoz initmikonam

--- main.js
+++ main.js
@@ -7,17 +7,6 @@ const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 const bot = new TelegramBot(token, {polling: true});
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
 
 // Listen for any kind of message. There are different kinds of
 // messages.
