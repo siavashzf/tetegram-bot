aa9bb4bf4fb5594e8fe8e6ec5e6a8de04d5bb527
siavashzf <zarif.siavash.offical@gmail.com>
2020-12-09 02:43:58 +0330

    asds

--- webhook.js
+++ webhook.js
@@ -34,8 +34,28 @@ app.listen(port, () => {
 });
 let state=0;
 // Just to ping!
+
+
+
+
 bot.on('message', msg => {
-    if (state==0){bot.sendMessage(msg.chat.id, "0");
+    if (state==0){
+        const MongoClient = require('mongodb').MongoClient;
+        const uri = "mongodb+srv://siavash:Si@7257482@cluster0.oth6f.mongodb.net/telegram_bot?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+        
+            let myobj = { name: "Company Inc", address: "Highway 37" };
+            const collection = client.db("telegram_bot").collection("user").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+              });
+          // perform actions on the collection object
+            client.close();
+        });
+            
+    bot.sendMessage(msg.chat.id, "ok addd");
     state=1;
     }
     else{
