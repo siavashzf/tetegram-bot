ac5d27d825b612f8fdf8bf0b5a7cb80872f4d625
siavashzf <zarif.siavash.offical@gmail.com>
2020-12-15 20:12:33 +0330

    add call back test

--- config/key.js
+++ config/key.js
@@ -1,5 +1,6 @@
 module.exports={ 
     dataBaseUrl : "mongodb+srv://siavash:siavash7257482@cluster0.oth6f.mongodb.net/telegram_bot?retryWrites=true&w=majority",
-    token : '1431997595:AAEzLI8bXQiZX-JHC6ESy4LHmQf2yTgOzDc'
+    token : '1431997595:AAEzLI8bXQiZX-JHC6ESy4LHmQf2yTgOzDc',
+    adminChatId:196207259,
 
 };
