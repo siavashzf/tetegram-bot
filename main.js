d2f1097a1a0193c4b5b2b049a59f6894fb34adf8
siavashzf <zarif.siavash.offical@gmail.com>
2021-02-13 02:26:46 +0330

    fix bug

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const server=require("./server");
 const Db = require('./DataBase');
 const uri = require("./config/key").dataBaseUrl;
-const  Db  = require("./model/User");
+
 
 Db.connect(uri)
 .then(()=>{server.start()})
